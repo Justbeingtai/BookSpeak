2821834cea65f0299aedc877fd7d2f02ba4c68dd
Eric L <126723001+elavallee13@users.noreply.github.com>
2023-07-03 16:12:34 -0400

    Completed chat features, and setting up menu pages

--- public/chatscript.js
+++ public/chatscript.js
@@ -1,7 +1,6 @@
 const messageContainer = document.getElementById('message-container');
 const messageInput = document.getElementById('message-input');
-const sendButton = document.getElementById('send-button');
-const leaveButton = document.getElementById('leave-button');
+const sendIcon = document.getElementById('send-icon'); // Corrected this line
 
 // Adds a message to the message container
 function addMessage(message) {
@@ -19,21 +18,32 @@ function sendMessage() {
     // Need to replace the code below with Cheryls WebSocket
     addMessage(`You: ${message}`);
     messageInput.value = '';
+    sendIcon.innerHTML = '➤'; // Reset the icon
   }
 }
 
-sendButton.addEventListener('click', sendMessage);
+// Click event to send message
+sendIcon.addEventListener('click', sendMessage);
 
+// The feature when a user presses the enter/return key
 messageInput.addEventListener('keydown', function(event) {
-  if (event.keyCode === 13) {
+  if (event.key === 'Enter') {
+    event.preventDefault(); // Prevents a new line from being created when pressing Enter
     sendMessage();
   }
 });
 
-leaveButton.addEventListener('click', function() {
-  // Replace the code below with Cheryls WebSocket implementation
+// Code to change send icon
+messageInput.addEventListener('input', function() {
+  sendIcon.innerHTML = this.value.trim() !== '' ? '➤' : '➤';
+});
+const chatContainer = document.getElementById('chat-container');
+const leaveMessage = document.getElementById('leave-message');
 
-  messageContainer.innerHTML = '';
-  messageInput.value = '';
-  addMessage('You have left the chat.');
+leaveButton.addEventListener('click', function() {
+  // Hide the chat container
+  chatContainer.style.display = 'none';
+
+  // Show a "You've left the chat" message
+  leaveMessage.style.display = 'block';
 });
