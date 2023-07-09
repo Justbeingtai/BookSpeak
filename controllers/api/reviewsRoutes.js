e3d766a8c7e8a59d883734ef5672511e8649fd61
CherylHemphill <chemphill1208@gmail.com>
2023-07-08 22:40:15 -0500

    style

--- controllers/api/reviewsRoutes.js
+++ controllers/api/reviewsRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { Review } = require('../../models/Reviews');
 
 // Create a new review
-router.post('/', async (req, res) => {
+router.post('/reviews', async (req, res) => {
   try {
     const { userId, bookName, rating, comment } = req.body;
 
@@ -69,74 +69,4 @@ module.exports = router;
 
 
 
-// const express = require('express');
-// const router = express.Router();
-// const { Review, Book } = require('../../models/Reviews');
 
-// // Create a new review
-// router.post('/', async (req, res) => {
-//   try {
-//     const { bookId, userId, rating, comment } = req.body;
-    
-//     // Check if the book exists
-//     const book = await Book.findByPk(bookId);
-//     if (!book) {
-//       return res.status(404).json({ error: 'Book not found' });
-//     }
-
-//     // Create the review
-//     const review = await Review.create({
-//       bookId,
-//       userId,
-//       rating,
-//       comment,
-//     });
-
-//     res.status(201).json(review);
-//   } catch (error) {
-//     res.status(500).json({ error: 'Internal server error' });
-//   }
-// });
-
-// // Update a review
-// router.put('/:id', async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const { rating, comment } = req.body;
-
-//     const review = await Review.findByPk(id);
-//     if (!review) {
-//       return res.status(404).json({ error: 'Review not found' });
-//     }
-
-//     // Update the review
-//     review.rating = rating;
-//     review.comment = comment;
-//     await review.save();
-
-//     res.status(200).json(review);
-//   } catch (error) {
-//     res.status(500).json({ error: 'Internal server error' });
-//   }
-// });
-
-// // Delete a review
-// router.delete('/:id', async (req, res) => {
-//   try {
-//     const { id } = req.params;
-
-//     const review = await Review.findByPk(id);
-//     if (!review) {
-//       return res.status(404).json({ error: 'Review not found' });
-//     }
-
-//     // Delete the review
-//     await review.destroy();
-
-//     res.status(204).end();
-//   } catch (error) {
-//     res.status(500).json({ error: 'Internal server error' });
-//   }
-// });
-
-// module.exports = router;
---
+++ public/js/google.js
@@ -0,0 +1,35 @@
+// Fetch a searched book from the Google Books API
+const searchBooks = async (searchTerm) => {
+    try {
+      const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchTerm}`);
+      const data = await response.json();
+      return data.items;
+    } catch (error) {
+      console.error('Error fetching book data:', error);
+      return [];
+    }
+  };
+  
+  // Function to render the book data in the template
+  const renderBooks = (books) => {
+    const template = Handlebars.compile(document.getElementById('google-template').innerHTML);
+    const html = template({ books });
+    document.getElementById('book-results-container').innerHTML = html;
+  };
+  
+  // Function to handle the form submission
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+  
+    const searchInput = document.getElementById('search-input');
+    const searchTerm = searchInput.value.trim();
+  
+    if (searchTerm !== '') {
+      const books = await searchBooks(searchTerm);
+      renderBooks(books);
+    }
+  };
+  
+  // Event listener for the form submission
+  document.getElementById('search-form').addEventListener('submit', handleFormSubmit);
+  
